976e79b455c06edac380e68d7969a88b18d90049
MorSof <morsof48@gmail.com>
2024-04-07 18:46:03 +0300

    wip

--- src/js/slides/video.js
+++ src/js/slides/video.js
@@ -42,6 +42,12 @@ export default function slideVideo(slide, data, index, callback) {
             provider = 'youtube';
         }
 
+        // TODO if url include 'kaltura', the provider should be kaltura
+        // Set kaltura videos
+        if (!provider && url.match(/kaltura/)) {
+            provider = 'kaltura';
+        }
+
         // Set local videos
         // if no provider, default to local
         if (provider === 'local' || !provider) {
@@ -58,23 +64,36 @@ export default function slideVideo(slide, data, index, callback) {
             customPlaceholder = createHTML(html);
         }
 
+        let placeholder;
+        if (provider === 'kaltura') {
+            const playerContainerId = 'kaltura_player_573480599';
+            customPlaceholder = createHTML(`<div id="${playerContainerId}" class="gvideo-local" style="background:#000; max-width: ${data.width}; height: ${data.height || '640px'}" data-plyr-provider="${provider}" data-plyr-embed-id="${url}"></div>`);
+        }
+
         // prettier-ignore
-        const placeholder = customPlaceholder ? customPlaceholder : createHTML(`<div id="${videoID}" data-plyr-provider="${provider}" data-plyr-embed-id="${url}"></div>`);
+        placeholder = customPlaceholder ? customPlaceholder : createHTML(`<div id="${videoID}" data-plyr-provider="${provider}" data-plyr-embed-id="${url}"></div>`);
 
         addClass(videoWrapper, `${provider}-video gvideo`);
         videoWrapper.appendChild(placeholder);
         videoWrapper.setAttribute('data-id', videoID);
         videoWrapper.setAttribute('data-index', index);
 
-        const playerConfig = has(this.settings.plyr, 'config') ? this.settings.plyr.config : {};
-        const player = new Plyr('#' + videoID, playerConfig);
+        if (provider === 'kaltura') {
+            playKalturaVideo();
+            callback();
+        } else {
+            const playerConfig = has(this.settings.plyr, 'config') ? this.settings.plyr.config : {};
+            const player = new Plyr('#' + videoID, playerConfig);
 
-        player.on('ready', (event) => {
-            videoPlayers[videoID] = event.detail.plyr;
-            if (isFunction(callback)) {
-                callback();
-            }
-        });
+            player.on('ready', (event) => {
+                videoPlayers[videoID] = event.detail.plyr;
+                if (isFunction(callback)) {
+                    callback();
+                }
+            });
+            player.on('enterfullscreen', handleMediaFullScreen);
+            player.on('exitfullscreen', handleMediaFullScreen);
+        }
         waitUntil(
             () => {
                 return slide.querySelector('iframe') && slide.querySelector('iframe').dataset.ready == 'true';
@@ -83,8 +102,6 @@ export default function slideVideo(slide, data, index, callback) {
                 this.resize(slide);
             }
         );
-        player.on('enterfullscreen', handleMediaFullScreen);
-        player.on('exitfullscreen', handleMediaFullScreen);
     });
 }
 
@@ -101,5 +118,21 @@ function handleMediaFullScreen(event) {
     }
     if (event.type === 'exitfullscreen') {
         removeClass(media, 'fullscreen');
+    }
+}
+
+function playKalturaVideo() {
+    const playerContainerId = 'kaltura_player_573480599';
+    try {
+        const kalturaPlayer = KalturaPlayer.setup({
+            targetId: playerContainerId,
+            provider: {
+                partnerId: 4661612,
+                uiConfId: 50273112,
+            },
+        });
+        kalturaPlayer.loadMedia({ entryId: '1_qazfolmk' });
+    } catch (e) {
+        console.error('Error loading Kaltura Player:', e.message);
     }
 }
